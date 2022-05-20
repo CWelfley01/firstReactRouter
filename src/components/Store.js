efe8678bc96d09e4976718eb412875ea33db241f
cwelfley01 <cwelfley01@gmail.com>
2022-05-19 20:22:14 -0400

    update

--- src/components/Store.js
+++ src/components/Store.js
@@ -4,9 +4,11 @@ import styles from "../style/store.scss"
 
 export default function Store() {
   return (
-  <div className="store">Store
-    <h1>Mages Keep</h1>
-    <h2>Books, Scrolls, Spellwork and Ingredients</h2>
+  <div className="store">
+    <div className="store-banner">
+      <h1 className="store-title" >Mages Keep</h1>
+      <h2 className="store-title" >Books, Scrolls, Spellwork and Ingredients</h2>
+    </div>
     
   </div>
   );
---
+++ src/components/items.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function items() {
+    return (
+        <div>
+            <img src="placehold">placehold</img>
+        </div>
+    );}
